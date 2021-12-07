fd999dffba7e771b7f961d932f63458801b8de9f
SophieJones <sophie.jones@rockborne.com>
2021-12-07 11:16:06 +0000

    make some slight progress

--- day3.py
+++ day3.py
@@ -2,19 +2,19 @@
 import re
 with open('input3.txt') as f:
     input3 = f.readlines()
-#print(input3)
+print(input3)
 i = 0
 while i< len(input3):
     input3[i] = (re.sub("\n", "", input3[i]))
     i+=1
     
-#print(input3[0][0])
+print(input3)
 
 gammarate = []
 epsilonrate = []
-i = 0
 x=0
-while x <= len(input3[0]):
+while x < len(input3[0]):
+    i = 0
     onecount = 0
     zerocount = 0 
     while i< len(input3):
@@ -24,15 +24,19 @@ while x <= len(input3[0]):
         elif input3[i][x] =='0':
             zerocount +=1
         i+=1
-    #print(onecount)
-    #print(zerocount)
+    print(onecount)
+    print(zerocount)
 
     if onecount>zerocount :
         gammarate.append(1)
         epsilonrate.append(0)
+        print(gammarate)
+        print(epsilonrate)
     else:
         gammarate.append(0)
         epsilonrate.append(1)
+        print(gammarate)
+        print(epsilonrate)
     x +=1
 print(gammarate)
 print(epsilonrate)
@@ -41,6 +45,28 @@ gamma = int(''.join(str(i) for i in gammarate))
 print(gamma)
 epsilon = int(''.join(str(i) for i in epsilonrate))
 print(epsilon)
-print(gamma*epsilon)
 
+bnum1 = gamma
+dnum1 = 0
+i = 1
+while bnum1!=0:
+    rem1 = bnum1%10
+    dnum1 = dnum1 + (rem1*i)
+    i = i*2
+    bnum1 = int(bnum1/10)
 
+print("\n First Equivalent Decimal Value = ", dnum1)
+
+bnum2 = epsilon
+dnum2 = 0
+i = 1
+while bnum2!=0:
+    rem2 = bnum2%10
+    dnum2 = dnum2 + (rem2*i)
+    i = i*2
+    bnum2 = int(bnum2/10)
+
+print("\n Second Equivalent Decimal Value = ", dnum2)
+print("\n Value = ", dnum1*dnum2)
+
+#Task 2
